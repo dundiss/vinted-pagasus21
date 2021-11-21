cef4f125a88203f99835102600589645573a7448
Diakary TRAORE <diakarytraore@MacBook-Pro-de-Diakary.local>
2021-11-21 16:26:15 +0100

    Fixing product description and product title maximum allowed characters, Fixing product price range values (from 0 to 100000)

--- models/Offer.js
+++ models/Offer.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const Offer = mongoose.model("Offer", {
-    product_name: String,
-    product_description: String,
-    product_price: Number,
+    product_name: { type: String, maxLength: 50 },
+    product_description: { type: String, maxLength: 500 },
+    product_price: { type: Number, min: 0, max: 100000 },
     product_details: Array,
     product_image: { type: mongoose.Schema.Types.Mixed, default: {} },
     owner: {
