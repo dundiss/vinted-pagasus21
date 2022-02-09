a95112d4fb9c46516ea604cda44e9a19a4022766
Diakary TRAORE <diakarytraore@MacBook-Pro-de-Diakary.local>
2022-02-09 23:30:31 +0100

    Ajout d'une vérification de présence du champ authorization pour recuperer le token

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,15 +1,21 @@
 const User = require("../models/User");
 
 const isAuthenticated = async (req, res, next) => {
-    const token = req.headers.authorization.replace("Bearer ", "");
-    const user = await User.findOne({ token: token });
-    if (user) {
-        //console.log(user);
-        req.user = user;
-        next();
-    }
-    else {
-        res.status(401).json({ message: "Unauthorized" });
+    if (req.headers.authorization) {
+        const user = await User.findOne({
+            token: req.headers.authorization.replace("Bearer ", "")
+        });
+
+        if (!user) {
+            return res.status(401).json({ error: "Unauthorized" });
+        } else {
+            req.user = user;
+            // On crée une clé "user" dans req. La route dans laquelle le middleware est appelé 
+            // pourra avoir accès à req.user
+            return next();
+        }
+    } else {
+        return res.status(401).json({ error: "Unauthorized" });
     }
 };
 
